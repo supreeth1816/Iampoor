d7d6dba126776c9f6d80cd343b553e0bba98f8ab
supreeth1816 <supreet.venkata@gmail.com>
2020-08-27 23:21:39 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+
+      home: Scaffold(
+
+        backgroundColor: Colors.blue,
+
+        appBar: AppBar(
+
+          title: Text("I am Poor"),
+          backgroundColor: Colors.black,
+
+        ),
+
+        body: Center(
+
+        child: Text("I am Poor")
+
+        ),
+
+
+
+
+      )
+
+
+
+    )
+
+
+  );
+
+
+}
+
